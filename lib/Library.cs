0dd019458fc2920585b4f488f2d3fe14501029ad
Ark223 <31862947+Ark223@users.noreply.github.com>
2026-04-04 20:54:50 +0200

    Expose library version

---
+++ lib/Library.cs
@@ -0,0 +1,13 @@
+﻿namespace Ace
+{
+    /// <summary>
+    /// Exposes library version information.
+    /// </summary>
+    public static class Library
+    {
+        /// <summary>
+        /// Gets the current library version.
+        /// </summary>
+        public static string Version => "1.0.3";
+    }
+}
